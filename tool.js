1b6496a1c65a6b97b53b28bebb418fdb073c4c09
kuangyazhou <18707174713@139.com>
2017-11-22 16:47:21 +0800

    为了造轮子而重复造轮子

--- tool.js
+++ tool.js
@@ -12,3 +12,328 @@ function arrayEqual(arr1, arr2) {
     }
     return true
 }
+/**
+ * 
+ * @desc   为元素添加class
+ * @param  {HTMLElement} ele 
+ * @param  {String} cls 
+ */
+
+var hasClass = require('./hasClass');
+
+function addClass(ele, cls) {
+    if (!hasClass(ele, cls)) {
+        ele.className += ' ' + cls;
+    }
+}
+
+/**
+ * 
+ * @desc 判断元素是否有某个class
+ * @param {HTMLElement} ele 
+ * @param {String} cls 
+ * @return {Boolean}
+ */
+function hasClass(ele, cls) {
+    return (new RegExp('(\\s|^)' + cls + '(\\s|$)')).test(ele.className);
+}
+/**
+ * 
+ * @desc 为元素移除class
+ * @param {HTMLElement} ele 
+ * @param {String} cls 
+ */
+
+// var hasClass = require('./hasClass');
+
+function removeClass(ele, cls) {
+    if (hasClass(ele, cls)) {
+        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+        ele.className = ele.className.replace(reg, ' ');
+    }
+}
+
+/**
+ * 
+ * @desc 根据name读取cookie
+ * @param  {String} name 
+ * @return {String}
+ */
+function getCookie(name) {
+    var arr = document.cookie.replace(/\s/g, "").split(';');
+    for (var i = 0; i < arr.length; i++) {
+        var tempArr = arr[i].split('=');
+        if (tempArr[0] == name) {
+            return decodeURIComponent(tempArr[1]);
+        }
+    }
+    return '';
+}
+
+var setCookie = require('./setCookie');
+/**
+ * 
+ * @desc 根据name删除cookie
+ * @param  {String} name 
+ */
+function removeCookie(name) {
+    // 设置已过期，系统会立刻删除cookie
+    setCookie(name, '1', -1);
+}
+
+/**
+ * 
+ * @desc  设置Cookie
+ * @param {String} name 
+ * @param {String} value 
+ * @param {Number} days 
+ */
+function setCookie(name, value, days) {
+    var date = new Date();
+    date.setDate(date.getDate() + days);
+    document.cookie = name + '=' + value + ';expires=' + date;
+}
+
+
+/**
+ * 
+ * @desc 获取浏览器类型和版本
+ * @return {String} 
+ */
+function getExplore() {
+    var sys = {},
+        ua = navigator.userAgent.toLowerCase(),
+        s;
+    (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? sys.ie = s[1]:
+        (s = ua.match(/msie ([\d\.]+)/)) ? sys.ie = s[1] :
+        (s = ua.match(/edge\/([\d\.]+)/)) ? sys.edge = s[1] :
+        (s = ua.match(/firefox\/([\d\.]+)/)) ? sys.firefox = s[1] :
+        (s = ua.match(/(?:opera|opr).([\d\.]+)/)) ? sys.opera = s[1] :
+        (s = ua.match(/chrome\/([\d\.]+)/)) ? sys.chrome = s[1] :
+        (s = ua.match(/version\/([\d\.]+).*safari/)) ? sys.safari = s[1] : 0;
+    // 根据关系进行判断
+    if (sys.ie) return ('IE: ' + sys.ie)
+    if (sys.edge) return ('EDGE: ' + sys.edge)
+    if (sys.firefox) return ('Firefox: ' + sys.firefox)
+    if (sys.chrome) return ('Chrome: ' + sys.chrome)
+    if (sys.opera) return ('Opera: ' + sys.opera)
+    if (sys.safari) return ('Safari: ' + sys.safari)
+    return 'Unkonwn'
+}
+
+/**
+ * 
+ * @desc 获取操作系统类型
+ * @return {String} 
+ */
+function getOS() {
+    var userAgent = 'navigator' in window && 'userAgent' in navigator && navigator.userAgent.toLowerCase() || '';
+    var vendor = 'navigator' in window && 'vendor' in navigator && navigator.vendor.toLowerCase() || '';
+    var appVersion = 'navigator' in window && 'appVersion' in navigator && navigator.appVersion.toLowerCase() || '';
+
+    if (/mac/i.test(appVersion)) return 'MacOSX'
+    if (/win/i.test(appVersion)) return 'windows'
+    if (/linux/i.test(appVersion)) return 'linux'
+    if (/iphone/i.test(userAgent) || /ipad/i.test(userAgent) || /ipod/i.test(userAgent)) 'ios'
+    if (/android/i.test(userAgent)) return 'android'
+    if (/win/i.test(appVersion) && /phone/i.test(userAgent)) return 'windowsPhone'
+}
+
+/**
+ * 
+ * @desc 获取滚动条距顶部的距离
+ */
+function getScrollTop() {
+    return (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+}
+
+var keyCodeMap = {
+    8: 'Backspace',
+    9: 'Tab',
+    13: 'Enter',
+    16: 'Shift',
+    17: 'Ctrl',
+    18: 'Alt',
+    19: 'Pause',
+    20: 'Caps Lock',
+    27: 'Escape',
+    32: 'Space',
+    33: 'Page Up',
+    34: 'Page Down',
+    35: 'End',
+    36: 'Home',
+    37: 'Left',
+    38: 'Up',
+    39: 'Right',
+    40: 'Down',
+    42: 'Print Screen',
+    45: 'Insert',
+    46: 'Delete',
+
+    48: '0',
+    49: '1',
+    50: '2',
+    51: '3',
+    52: '4',
+    53: '5',
+    54: '6',
+    55: '7',
+    56: '8',
+    57: '9',
+
+    65: 'A',
+    66: 'B',
+    67: 'C',
+    68: 'D',
+    69: 'E',
+    70: 'F',
+    71: 'G',
+    72: 'H',
+    73: 'I',
+    74: 'J',
+    75: 'K',
+    76: 'L',
+    77: 'M',
+    78: 'N',
+    79: 'O',
+    80: 'P',
+    81: 'Q',
+    82: 'R',
+    83: 'S',
+    84: 'T',
+    85: 'U',
+    86: 'V',
+    87: 'W',
+    88: 'X',
+    89: 'Y',
+    90: 'Z',
+
+    91: 'Windows',
+    93: 'Right Click',
+
+    96: 'Numpad 0',
+    97: 'Numpad 1',
+    98: 'Numpad 2',
+    99: 'Numpad 3',
+    100: 'Numpad 4',
+    101: 'Numpad 5',
+    102: 'Numpad 6',
+    103: 'Numpad 7',
+    104: 'Numpad 8',
+    105: 'Numpad 9',
+    106: 'Numpad *',
+    107: 'Numpad +',
+    109: 'Numpad -',
+    110: 'Numpad .',
+    111: 'Numpad /',
+
+    112: 'F1',
+    113: 'F2',
+    114: 'F3',
+    115: 'F4',
+    116: 'F5',
+    117: 'F6',
+    118: 'F7',
+    119: 'F8',
+    120: 'F9',
+    121: 'F10',
+    122: 'F11',
+    123: 'F12',
+
+    144: 'Num Lock',
+    145: 'Scroll Lock',
+    182: 'My Computer',
+    183: 'My Calculator',
+    186: ';',
+    187: '=',
+    188: ',',
+    189: '-',
+    190: '.',
+    191: '/',
+    192: '`',
+    219: '[',
+    220: '\\',
+    221: ']',
+    222: '\''
+};
+/**
+ * @desc 根据keycode获得键名
+ * @param  {Number} keycode 
+ * @return {String}
+ */
+function getKeyName(keycode) {
+    if (keyCodeMap[keycode]) {
+        return keyCodeMap[keycode];
+    } else {
+        console.log('Unknow Key(Key Code:' + keycode + ')');
+        return '';
+    }
+};
+
+
+/**
+ * @desc 深拷贝，支持常见类型
+ * @param {Any} values
+ */
+function deepClone(values) {
+    var copy;
+
+    // Handle the 3 simple types, and null or undefined
+    if (null == values || "object" != typeof values) return values;
+
+    // Handle Date
+    if (values instanceof Date) {
+        copy = new Date();
+        copy.setTime(values.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (values instanceof Array) {
+        copy = [];
+        for (var i = 0, len = values.length; i < len; i++) {
+            copy[i] = deepClone(values[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (values instanceof Object) {
+        copy = {};
+        for (var attr in values) {
+            if (values.hasOwnProperty(attr)) copy[attr] = deepClone(values[attr]);
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy values! Its type isn't supported.");
+}
+
+/**
+ * 
+ * @desc 随机生成颜色
+ * @return {String} 
+ */
+function randomColor() {
+    return '#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).slice(-6);
+}
+
+/**
+ * 
+ * @desc   判断是否为邮箱地址
+ * @param  {String}  str
+ * @return {Boolean} 
+ */
+function isEmail(str) {
+    return /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(str);
+}
+
+/**
+ * 
+ * @desc  判断是否为身份证号
+ * @param  {String|Number} str 
+ * @return {Boolean}
+ */
+function isIdCard(str) {
+    return /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/.test(str)
+}
